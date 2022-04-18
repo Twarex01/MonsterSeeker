e98377e5d16ea012627297b975e21cd9f0bfd500
Laki Balázs <twarex01@gmail.com>
2022-04-18 19:44:08 +0200

    Fix

--- Project/app/src/main/java/com/example/monsterseeker/viewmodels/DetailedMonsterViewModel.kt
+++ Project/app/src/main/java/com/example/monsterseeker/viewmodels/DetailedMonsterViewModel.kt
@@ -2,12 +2,20 @@ package com.example.monsterseeker.viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.monsterseeker.models.DetailedMonster
 
-public class DetailedMonsterViewModel (
-    private val detailedMonsters : MutableLiveData<List<DetailedMonster>>
-) {
+public class DetailedMonsterViewModel : ViewModel() {
+    private val detailedMonsters: MutableLiveData<List<DetailedMonster>> by lazy {
+        MutableLiveData<List<DetailedMonster>>().also {
+            loadDetailedMonsters()
+        }
+    }
+
     fun getDetailedMonsters(): LiveData<List<DetailedMonster>> {
         return detailedMonsters
+    }
+
+    private fun loadDetailedMonsters() {
     }
 }
--- Project/app/src/main/java/com/example/monsterseeker/viewmodels/ListMonsterViewModel.kt
+++ Project/app/src/main/java/com/example/monsterseeker/viewmodels/ListMonsterViewModel.kt
@@ -2,12 +2,20 @@ package com.example.monsterseeker.viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.monsterseeker.models.ListMonster
 
-public class ListMonsterViewModel(
-    private val listMonsters : MutableLiveData<List<ListMonster>>
-) {
-    fun getListMonsters(): LiveData<List<ListMonster>> {
+public class ListMonsterViewModel : ViewModel() {
+    private val listMonsters: MutableLiveData<List<ListMonster>> by lazy {
+        MutableLiveData<List<ListMonster>>().also {
+            loadListMonsters()
+        }
+    }
+
+    fun getDetailedMonsters(): LiveData<List<ListMonster>> {
         return listMonsters
+    }
+
+    private fun loadListMonsters() {
     }
 }
